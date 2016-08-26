2a4c6984f7e35d3fca5cefc3b7b69bc710722d95
Katie Tuttle <katka.karolcikova@gmail.com>
2016-08-26 11:42:16 -0700

    display number of questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  bestAnswer: Ember.computed('model.@each.best', {
+    get: function() {
+      return this.get('model').filterBy('best', true).length;
+    },
+  }),
+
   actions: {
+    best: function(answer) {
+      answer.toggleProperty('best');
+    },
     destroyQuestion(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
